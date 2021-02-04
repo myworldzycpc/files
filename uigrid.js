c50a7be1fa5648a36a910a5d1765a215ed29c2b0
myworldzycpc <67212814+myworldzycpc@users.noreply.github.com>
2021-02-04 10:51:52 +0800

    Update uigrid.js

--- uigrid.js
+++ uigrid.js
@@ -1,34 +1,31 @@
-$(document).ready(function(){
-	function initgrid(width,height,border){
-		$table = $("<table></table>");
+function initgrid(width,height,border){
+	$table = $("<table></table>");
 		
-		for (var x = 0; x < width; x++){
-			$tr = $("<tr></tr>");
-			for (var y = 0; y < height; y++){
-				$td = $("<td></td>");
-				if (border){
-					$td.css({"border":"1px solid black"});
-				}
-				$tr.append($td);
-			};
-			$table.append($tr);
-			
+	for (var x = 0; x < width; x++){
+		$tr = $("<tr></tr>");
+		for (var y = 0; y < height; y++){
+			$td = $("<td></td>");
+			if (border){
+				$td.css({"border":"1px solid black"});
+			}
+			$tr.append($td);
 		};
-		$table.attr({"class":"uigrid"});
+		$table.append($tr);
+			
+	};
+	$table.attr({"class":"uigrid"});
 		
-		$("body").append($table);
-	};
+	$("body").append($table);
+};
 	
-	function getblock(x,y){
-		return $(".uigrid").children().children().eq(y).children().eq(x);
-		
-	};
+function getblock(x,y){
+	return $(".uigrid").children().children().eq(y).children().eq(x);
+};
 	
-	function createlement(tagname){
-		return $(`<${tagname}></${tagname}>`);
-	};
-	var ing = initgrid;
-	var gbl = initgrid;
-	var cel = createlement;
+function createlement(tagname){
+	return $(`<${tagname}></${tagname}>`);
+};
 	
-});
+var ing = initgrid;
+var gbl = getblock;
+var cel = createlement;
